b38444e4fdabda00d95657c5baa2bab95ffd6258
bas1c <68533770+bas1c1@users.noreply.github.com>
2022-12-24 16:38:53 +0300

    v1.0

---
+++ src/Sprite.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable CA1416
+namespace FWD
+{
+    public class Sprite
+    {
+        private string name;
+        private int x, y;
+        private Bitmap sprite;
+
+        public Sprite(int x, int y, Bitmap sprite, string name = "")
+        {
+            this.x = x;
+            this.y = y;
+            this.sprite = sprite;
+            this.name = name;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public Bitmap _Sprite { get => sprite; set => sprite = value; }
+        public string Name { get => name; set => name = value; }
+
+        public static Sprite fromFile(string name, int x, int y)
+        {
+            System.Drawing.Bitmap bitmap = (System.Drawing.Bitmap)System.Drawing.Image.FromFile(name);
+            return new Sprite(x, y, Bitmap.fromGdiBitmap(bitmap), name);
+        }
+    }
+}
